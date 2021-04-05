885ae4a5ca53a816ac5d7ff417c60b65041c047b
zdicken <78879218+zdicken@users.noreply.github.com>
2021-04-04 23:54:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+new Vue({
+  el: "#destiny-app",
+  data: {
+    cards:[
+      {
+        name:"Rey",
+        rarity:false,
+        setNumber:100,
+        affl:"hero",
+        color:"blue",
+        health:10,
+        img:"https://swdestinydb.com/bundles/cards/en/01/01038.jpg"
+      },
+      {
+        name:"Han Solo",
+        rarity:true,
+        setNumber:36,
+        affl:"hero",
+        color:"yellow",
+        health:10,
+        img:"http://images-cdn.fantasyflightgames.com/filer_public/3b/15/3b1513ab-1bf2-46f9-93ec-9fb0246e567b/sws17_main.png"
+      }
+    ]
+  }
+})
